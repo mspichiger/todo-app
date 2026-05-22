be5abdbf1736eb8f9e863f1e1f3dbadeb3a9f049
spichigm <mara.spichiger@roche.com>
2026-05-22 09:28:28 +0200

    fix: angular test @angular router

--- todo-app/src/app/todo-detail/todo-detail.spec.ts
+++ todo-app/src/app/todo-detail/todo-detail.spec.ts
@@ -1,30 +1,17 @@
 import { TestBed } from '@angular/core/testing';
-import { provideRouter } from '@angular/router';
+import { Router, provideRouter } from '@angular/router';
 import { RouterTestingHarness } from '@angular/router/testing';
 
 import { TodoDetail } from './todo-detail';
 
 describe('TodoDetail', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      providers: [
-        provideRouter([
-          { path: 'todos/:id', component: TodoDetail },
-        ]),
-      ],
-    }).compileComponents();
-  });
-
-  it('should create', async () => {
-    const harness = await RouterTestingHarness.create();
-    const component = await harness.navigateByUrl('/todos/ABC', TodoDetail);
-    expect(component).toBeTruthy();
-  });
-
-  it('should render the id from the route parameter in the DOM', async () => {
+  it('should display todo ID from route parameters', async () => {
+    TestBed.configureTestingModule({
+      imports: [TodoDetail],
+      providers: [provideRouter([{ path: 'todos/:id', component: TodoDetail }])],
+    });
     const harness = await RouterTestingHarness.create();
     await harness.navigateByUrl('/todos/ABC', TodoDetail);
-
     expect(harness.routeNativeElement?.textContent).toContain('ABC');
   });
 });
