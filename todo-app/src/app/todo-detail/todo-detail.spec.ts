bb51f0d0f03cfd50d7bd1be56afbbcc4d77005dc
spichigm <mara.spichiger@roche.com>
2026-05-28 12:51:48 +0200

    feat: add lint

--- todo-app/src/app/todo-detail/todo-detail.spec.ts
+++ todo-app/src/app/todo-detail/todo-detail.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed } from '@angular/core/testing';
-import { Router, provideRouter } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { RouterTestingHarness } from '@angular/router/testing';
 
 import { TodoDetail } from './todo-detail';
