0ab68e58c861af0b4de2478217005adc283dc3c6
spichigm <mara.spichiger@roche.com>
2026-05-28 11:03:11 +0200

    feat: form validation

--- todo-app/src/app/app.routes.server.ts
+++ todo-app/src/app/app.routes.server.ts
@@ -1,6 +1,8 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
+  { path: 'todos/new', renderMode: RenderMode.Server },
+  { path: 'todos/:id/edit', renderMode: RenderMode.Server },
   {
     path: '**',
     renderMode: RenderMode.Prerender
