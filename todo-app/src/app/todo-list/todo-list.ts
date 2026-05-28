70ae1be6f17a3dd67e98c109452a3f6edabae6cf
spichigm <mara.spichiger@roche.com>
2026-05-28 13:19:01 +0200

    fix: delete route 42

--- todo-app/src/app/todo-list/todo-list.ts
+++ todo-app/src/app/todo-list/todo-list.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-todo-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './todo-list.html',
   styleUrl: './todo-list.css',
 })
-export class TodoList {}
+export class TodoList { }
